448bfd34ce0b918c600f4748cba5e28543060cfd
LucasFlaquer <lucas.flaquer@gmail.com>
2023-11-15 13:55:52 -0300

    migration: create table meals

---
+++ database/migrations/20231114220050_create-meals.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description')
+    table.dateTime('created_at')
+    table.boolean('on_diet')
+    table.uuid('user_id')
+    table.foreign('user_id').references('id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
